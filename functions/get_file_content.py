dbd21581decddd2d02ac4b859dced9d87a92d827
AgentM07-gh <michael.gent@gmail.com>
2025-07-30 18:24:15 -0400

    Ch2L3

---
+++ functions/get_file_content.py
@@ -0,0 +1,29 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    actual_file_path = os.path.join(working_directory, file_path)
+    abs_file_path = os.path.abspath(actual_file_path)
+    print(abs_file_path)
+    abs_workdir = os.path.abspath(working_directory)
+    print(abs_workdir)
+    if not abs_file_path.startswith(abs_workdir):
+        return f'Error: Cannot read "{abs_file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{abs_file_path}"'
+    
+    try:
+        with open(abs_file_path, "r") as f:
+            file = f.read()
+            if len(file) > MAX_CHARS:
+                file = file[:MAX_CHARS]
+                file += (f'[...File "{abs_file_path}" truncated at {MAX_CHARS} characters]')
+                #print(len(file))
+    except FileNotFoundError:
+        return "Error: The specified file was not found."
+    except PermissionError:
+        return "Error: You do not have permission to access this file."
+    except IOError as e:
+        return f"Error: An I/O error occurred: {e}"
+    
+    return file
--- tests.py
+++ tests.py
@@ -1,13 +1,14 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print("Result for current directory:")
-print(get_files_info("calculator", "."))
+print("Result for lorem.txt:")
+print(get_file_content("calculator", "main.py"))
 
-print("Result for 'pkg' directory:")
-print(get_files_info("calculator", "pkg"))
+print("Result for 'pkg/calculator.py':")
+print(get_file_content("calculator", "pkg/calculator.py"))
 
-print("Result for '/bin' directory:")
-print(get_files_info("calculator", "/bin"))
+print("Result for '/bin/cat':")
+print(get_file_content("calculator", "/bin/cat"))
 
-print("Result for '../' directory:")
-print(get_files_info("calculator", "../"))
+print("Result for 'pkg/does_not_exist.py")
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
